f1eb96688f99fc379398bf8289e4d88e00af117a
Pedro Santos <90013568+PedroSantoss-Dev@users.noreply.github.com>
2023-03-26 22:48:40 -0300

    fix: restructuring

--- src/pages/userById/index.tsx
+++ src/pages/userById/index.tsx
@@ -3,54 +3,79 @@ import api from "../../shared/service/api";
 import * as S from "./styled";
 import { useParams } from "react-router-dom";
 import { IUser } from "../../@types/indes";
-import axios from "axios";
 import { TabBar } from "../../shared/components/tabsBar";
 
 
 export const UserById = () => {
-    const [ user, setUser ] = useState<IUser>();
-    const [ avatar, setAvatar ] = useState<any>([])
-
-    const {id} = useParams();
-    const getUserById = async() => {
-        await api.get(`users/${id}`)
-        .then((response) => {
-            setUser(response.data)
-        })
+  const [user, setUser] = useState<IUser>({
+    id: 0,
+    name: '',
+    username: '',
+    email: '',
+    address: {
+      street: '',
+      suite: '',
+      city: '',
+      zipcode: '',
+      geo: {
+        lat: '',
+        lng: ''
+      }
+    },
+    phone: '',
+    website: '',
+    company: {
+      name: '',
+      catchPhrase: '',
+      bs: ''
     }
-    const Avatar = async() => {
-        await axios.get('https://dog.ceo/api/breeds/image/random')
-        .then((response) => {
-           setAvatar(response.data)
-       })
+  });
+  const [avatar, setAvatar] = useState<string>();
+ 
+  const { id } = useParams<{ id: string }>();
+
+  const getUserById = async () => {
+    try {
+      const response = await api.get(`users/${id}`);
+      setUser(response.data);
+    } catch (error) {
+      console.log(error);
     }
-    useEffect(() => {
-        getUserById();
-        Avatar()
-    },[])
-    return(
-        <S.Container>
-            <S.avatar src={avatar.message}/>
-            <h1>{user?.username}</h1>
-            <p>{user?.email}</p>
-            <p>{user?.phone}</p>
-            <p>{user?.website}</p>
-             <TabBar
-             userId={user?.id}
-             city={user?.address.city}
-             suite={user?.address.suite}
-             street={user?.address.street}
-             zipcode={user?.address.zipcode}
-             lat={user?.address.geo.lat}
-             lng={user?.address.geo.lng}
-             name={user?.company.name}
-             catchPhrase={user?.company.catchPhrase}
-             bs={user?.company.bs}
+  };
 
+  const getAvatar = async () => {
+    try {
+      const response = await api.get('https://dog.ceo/api/breeds/image/random');
+      setAvatar(response.data.message);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-             
-             />
-            
-        </S.Container>
-    )
-}
+  useEffect(() => {
+    getUserById();
+    getAvatar();
+  }, []);
+
+  return (
+    <S.Container>
+      <S.avatar src={avatar} />
+      <h1>{user?.username}</h1>
+      <p>{user?.email}</p>
+      <p>{user?.phone}</p>
+      <p>{user?.website}</p>
+      <TabBar
+        userId={user?.id}
+        city={user?.address.city}
+        suite={user?.address.suite}
+        street={user?.address.street}
+        zipcode={user?.address.zipcode}
+        lat={user?.address.geo.lat}
+        lng={user?.address.geo.lng}
+        name={user?.company.name}
+        catchPhrase={user?.company.catchPhrase}
+        bs={user?.company.bs}
+      />
+    </S.Container>
+  );
+};
